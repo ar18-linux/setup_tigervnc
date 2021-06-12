45d03e064dd4e73488b28fbb12647454a38bf576
nulysses <adr.zivil@gmail.com>
2021-06-12 10:54:03 +0200

    1

--- setup_tigervnc/setup_tigervnc.sh
+++ setup_tigervnc/setup_tigervnc.sh
@@ -6,12 +6,20 @@ LD_PRELOAD=
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+. "${script_dir}/vars"
+
 read -s -p "foo:" foo2
 
 echo "${foo2}" | sudo -S -k pacman -Sy --noconfirm tigervnc
 
+echo "Enter vnc password:"
+
 vncpasswd
 
-#. "${script_dir}/vars"
+mkdir -p "~/.vnc"
+cp -f "${script_dir}/xstartup" "~/.vnc/xstartup"
+
+echo "${foo2}" | sudo -Sk cp -f "${script_dir}/vncserver.service" "/etc/systemd/system/vncserver.service"
+echo "${foo2}" | sudo -Sk systemctl enable vncserver
 
 set +ex
